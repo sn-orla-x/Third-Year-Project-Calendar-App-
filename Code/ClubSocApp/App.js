c3259b08b014fa9af787c9793bf3e990a7cc0a6e
Orla Kinsella <orla.kinsella3@mail.dcu.ie>
2019-02-05 16:51:15 +0000

    intial commit

---
+++ Code/ClubSocApp/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style= {styles.mint}>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#696969',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  mint: {
+
+    color: '#aaf0d1',
+    fontWeight: 'bold',
+  },
+
+});
