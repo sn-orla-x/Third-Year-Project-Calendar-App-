eca3be7307bffdb32ab55980d54b3b619f77e6ce
Orla Kinsella <orla.kinsella3@mail.dcu.ie>
2019-02-06 12:52:35 +0000

    updates to UI as seen in App.js

--- Code/ClubSocApp/App.js
+++ Code/ClubSocApp/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, AppRegistry, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style= {styles.mint}>Open up App.js to start working on your app!</Text>
+      //<View style={styles.container}>
+        //<View style = {{flex: 1, backgroundColor: '#aaf0d1'}} />
+      <View style={{flex:1}}>
+        <View style={{height: 75, backgroundColor: '#aad0d1'}}>
+          <Text style={styles.mint}>"Hi there!"</Text>
+        </View>
+        <View style={{flex: 2, backgroundColor: '#696969'}} />
+        <View style={{flex: 3, backgroundColor: '#696969'}} />
       </View>
+        //<Text style={styles.mint}>Open up App.js to start working on your app!</Text>
+      //</View>
     );
   }
 }
@@ -21,8 +29,12 @@ const styles = StyleSheet.create({
 
   mint: {
 
-    color: '#aaf0d1',
+    color: '#696969',
     fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'left',
+    textAlignVertical: 'center'
   },
 
 });
