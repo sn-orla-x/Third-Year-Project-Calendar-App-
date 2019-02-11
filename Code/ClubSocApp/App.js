dcb2378b79619668b8214d7c4f2527717eace1d3
Orla Kinsella <orla.kinsella3@mail.dcu.ie>
2019-02-11 12:13:45 +0000

    more ui changes
    ui changes

--- Code/ClubSocApp/App.js
+++ Code/ClubSocApp/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, AppRegistry, View } from 'react-native';
+import { StyleSheet, Text, AppRegistry, View, Button, Alert } from 'react-native';
 
 export default class App extends React.Component {
+
+  _onPressButton() {
+    Alert.alert('tippy tap tip')
+  }
   render() {
     return (
       //<View style={styles.container}>
         //<View style = {{flex: 1, backgroundColor: '#aaf0d1'}} />
       <View style={{flex:1}}>
         <View style={{height: 75, backgroundColor: '#aad0d1'}}>
-          <Text style={styles.mint}>"Hi there!"</Text>
+          <Text style = {styles.mint}>DCU C&S</Text>
+          <View style={styles.butt}>
+            <Button
+             onPress={this._onPressButton}
+              title="Press Me"
+              color="#aad0d1"
+            /> 
+            <Button
+             onPress={this._onPressButton}
+              title="Press Me"
+              color="#aad0d1"
+            /> 
+          </View>
         </View>
         <View style={{flex: 2, backgroundColor: '#696969'}} />
-        <View style={{flex: 3, backgroundColor: '#696969'}} />
       </View>
-        //<Text style={styles.mint}>Open up App.js to start working on your app!</Text>
-      //</View>
+
     );
   }
 }
@@ -28,13 +42,33 @@ const styles = StyleSheet.create({
   },
 
   mint: {
-
-    color: '#696969',
-    fontWeight: 'bold',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    color: '#fff',
+    fontSize: 25,
+    //fontWeight: 'bold',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     textAlign: 'left',
-    textAlignVertical: 'center'
+    textAlignVertical: 'bottom',
+
+
+
   },
+  butt: {
+    flex: 1,
+    margin: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    textAlign: 'left',
+    textAlignVertical: 'bottom',
+    flex: 1,
+    color: '#fff',
+
+
+
+  }
+
+
+
 
 });
