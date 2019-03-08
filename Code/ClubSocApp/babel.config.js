5278e8558308e8c3e5a6f8965d103673b9e3dade
Orla Kinsella <orla.kinsella3@mail.dcu.ie>
2019-03-08 16:57:16 +0000

    final pre-bug fixing commit

--- Code/ClubSocApp/babel.config.js
+++ Code/ClubSocApp/babel.config.js
@@ -1,20 +1,22 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-  "presets": ["@babel/preset-env","@babel/preset-react","babel-preset-expo"],
+  "presets": ["@babel/preset-env","@babel/preset-react", "babel-preset-expo", "module:metro-react-native-babel-preset"],
     "plugins": [
       "react-hot-loader/babel",
       "dynamic-import-webpack",
-    "@babel/plugin-proposal-object-rest-spread"
+    "@babel/plugin-proposal-object-rest-spread",
+
  
     ],
   "env":{
     "test":{
-      "presets": ["@babel/preset-env","@babel/preset-react","babel-preset-expo"],
+      "presets": ["@babel/preset-env","@babel/preset-react", "babel-preset-expo","module:metro-react-native-babel-preset"],
     "plugins": [
       "react-hot-loader",
       "dynamic-import-webpack",
-    "@babel/plugin-proposal-object-rest-spread"
+    "@babel/plugin-proposal-object-rest-spread",
+ 
 
     ]}
   }
