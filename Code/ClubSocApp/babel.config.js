819750face251245ea919613ce5c762c7de1c254
Orla Kinsella <orla.kinsella3@mail.dcu.ie>
2019-03-08 11:41:29 +0000

    app no longer breaks expo

--- Code/ClubSocApp/babel.config.js
+++ Code/ClubSocApp/babel.config.js
@@ -1,20 +1,21 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-  "presets": ["@babel/preset-env","@babel/preset-react"],
+  "presets": ["@babel/preset-env","@babel/preset-react","babel-preset-expo"],
     "plugins": [
       "react-hot-loader/babel",
-      "transform-object-rest-spread",
-      "transform-class-properties",
-      "dynamic-import-webpack"
+      "dynamic-import-webpack",
+    "@babel/plugin-proposal-object-rest-spread"
+ 
     ],
   "env":{
     "test":{
-      "presets": ["@babel/preset-env","@babel/preset-react"],
+      "presets": ["@babel/preset-env","@babel/preset-react","babel-preset-expo"],
     "plugins": [
-      "transform-object-rest-spread",
-      "transform-class-properties",
-      "dynamic-import-webpack"
+      "react-hot-loader",
+      "dynamic-import-webpack",
+    "@babel/plugin-proposal-object-rest-spread"
+
     ]}
   }
 }
